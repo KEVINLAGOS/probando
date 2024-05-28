86d1f0e3e61eaa16a51f9dc95c3791c4259e668d
KEVINLAGOS <kevinlagos977@gmail.com>
2024-05-28 10:15:47 -0500

    probando

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
+const cors = require('cors');
 const { SwaggerTheme, SwaggerThemeNameEnum } = require('swagger-themes');
 const path = require('path');
 const fs = require('fs');
 const yaml = require('yamljs');
 const ternurinesRouter = require('../routes/ternurines.js');
 const envar = require('./config.js');
-
+app.use(cors());
 const app = express();
 const port = envar.PORT || 3000;
 const swaggerDocs = yaml.load(path.join(__dirname, '../docs/apidocs.yaml'));
